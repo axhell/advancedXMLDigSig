c0f2d000621d4f096a0cc29922e15ef1e33c59b0
axhell <edro.hi.ammen@gmail.com>
2015-01-25 02:00:21 +0100

    add main class  for test

---
+++ XMLDigSig/src/TestDigSig.java
@@ -0,0 +1,14 @@
+
+public class TestDigSig {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		GenDetachedBuilder inputs = new GenDetachedBuilder(args[0], args[1], args[2]);
+
+	}
+
+}
