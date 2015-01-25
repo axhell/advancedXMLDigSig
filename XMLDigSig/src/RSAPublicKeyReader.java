0c304ac88cc40a2e297dbec66cd7a33427cdd60b
axhell <edro.hi.ammen@gmail.com>
2015-01-25 11:45:30 +0100

    add try/catch exception

--- XMLDigSig/src/RSAPublicKeyReader.java
+++ XMLDigSig/src/RSAPublicKeyReader.java
@@ -13,17 +13,43 @@ public class RSAPublicKeyReader {
 	public static PublicKey getPubKeyFormFile(String filename) throws Exception{
 		
 		File f = new File(filename);
-		FileInputStream fis = new FileInputStream(f);
-		DataInputStream dis = new DataInputStream(fis);
-		byte[] keyBytes = new byte[(int)f.length()];
-		dis.readFully(keyBytes);
-		dis.close();
+		FileInputStream fis = null;
+		byte[] keyBytes = null;
+		try {
+			fis = new FileInputStream(f);
+			
+			try {
+				DataInputStream dis = new DataInputStream(fis);
+				keyBytes = new byte[(int)f.length()];
+				dis.readFully(keyBytes);
+				
+			} catch (Exception e) { e.printStackTrace(); } 
+			
+		} catch (IOException e){
+			// handle exception
+			e.printStackTrace();
+		} finally { 
+		    try {
+		        
+				if (fis != null) {
+		            fis.close();
+		        }        
+		    } catch (IOException e) {
+		        // handle exception
+		    	e.printStackTrace();
+		    }
+		}
+		
+		
 		
 		//X509 Encoding
 		X509EncodedKeySpec spec = new X509EncodedKeySpec(keyBytes);
 		//Generate public key
 		KeyFactory kf = KeyFactory.getInstance("RSA");
 		return kf.generatePublic(spec);
+		
+
+		
 	}
 	
 	/**
