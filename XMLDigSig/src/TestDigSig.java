9378787792010a4aa7ae7f50c1060ac8fb6d1e1f
axhell <edro.hi.ammen@gmail.com>
2015-01-25 11:48:56 +0100

    add basic input check, add encode  pub/priv key input before instantiating a GenDetachedBuilder object

--- XMLDigSig/src/TestDigSig.java
+++ XMLDigSig/src/TestDigSig.java
@@ -1,13 +1,46 @@
+import java.io.File;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+
 
 public class TestDigSig {
 
 	/**
 	 * @param args
+	 * @throws Exception 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		
-		GenDetachedBuilder inputs = new GenDetachedBuilder(args[0], args[1], args[2]);
+		PublicKey pubKey = null;
+		PrivateKey privKey = null;
+		String target = null;
+		
+		if (args[0] != null){
+			System.out.println("Public Key input field MUST NOT be empty");
+		}else{
+			pubKey = RSAPublicKeyReader.getPubKeyFormFile(args[0]);
+		}
+		
+		if (args[1] != null){
+			System.out.println("Private Key input field MUST NOT be empty");
+		}else{
+			privKey = RSAPrivateKeyReader.getPrivKeyFromFile(args[1]);
+		}
+		
+		if (args[2] != null){
+			System.out.println("Target of signature URI field MUST NOT be empty");
+		}else{
+			target = args[2];
+		}
+		
+		
+		
+		GenDetachedBuilder inputs = new GenDetachedBuilder(pubKey, privKey, target);
+		
+		inputs.GenerateSig();
 
 	}
 
