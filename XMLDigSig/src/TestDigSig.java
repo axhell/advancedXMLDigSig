1b60264d3e7ac7eada8508a7c287cc96135e9034
axhell <edro.hi.ammen@gmail.com>
2015-01-26 01:15:55 +0100

    input check

--- XMLDigSig/src/TestDigSig.java
+++ XMLDigSig/src/TestDigSig.java
@@ -18,29 +18,31 @@ public class TestDigSig {
 		PrivateKey privKey = null;
 		String target = null;
 		
-		if (args[0] != null){
+		if (args.length > 0 && args[0].length() > 0){
 			pubKey = RSAPublicKeyReader.getPubKeyFormFile(args[0]);
 		}else{
 			System.out.println("Public Key input field MUST NOT be empty");
 		}
 		
-		if (args[1] != null){
+		if (args.length > 1 && args[1] != null){
 			privKey = RSAPrivateKeyReader.getPrivKeyFromFile(args[1]);
 		}else{
 			System.out.println("Private Key input field MUST NOT be empty");
 		}
 		
-		if (args[2] != null){
+		if (args.length > 2 && args[2] != null){
 			target = args[2];
 		}else{
 			System.out.println("Target of signature URI field MUST NOT be empty");
 		}
 		
 		
+		if (args.length == 3){
+			GenDetachedBuilder inputs = new GenDetachedBuilder(pubKey, privKey, target);
+			inputs.GenerateSig();
+		}
 		
-		GenDetachedBuilder inputs = new GenDetachedBuilder(pubKey, privKey, target);
 		
-		inputs.GenerateSig();
 
 	}
 
