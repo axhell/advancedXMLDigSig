26204552e9b8ceadc2a4077cb68f54b99041c432
axhell <edro.hi.ammen@gmail.com>
2015-01-27 12:34:40 +0100

    create method RequestCMInstanceCreation

--- XMLDigSig/src/xmlsigmodule/InputValidationModule.java
+++ XMLDigSig/src/xmlsigmodule/InputValidationModule.java
@@ -1,21 +1,44 @@
 package xmlsigmodule;
 
-import xmlsigcore;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+
+import xmlsigcore.*;
 
 public class InputValidationModule extends TestDigSig{
 
-	/**
-	 * @param args
-	 */
-	public static boolean AuthorizationCMInstanceCreation(String cmTsignature, String cmTpubk, String fwpubk) {
+	
+/**
+ * 
+ * @param cmTsignature , signature of Certification Model template
+ * @param pubkcmT	,	Public Key for Certification Model validation
+ * @param pubkFW	,	Public Key of Framework that must be certified
+ * @return
+ * @throws Exception 
+ */
+	public static boolean RequestCMInstanceCreation(String cmTsignature, String pubkCMt, String pubkFW) throws Exception {
 		//
-		boolean procede = false;
+		boolean auth = false;
+		PublicKey pubKeyCMt = null;
+		PublicKey pubKeyFW = null;
+		String target = null;
 		
-		RSAPublicKeyReader.getPubKeyFormFile("ert");
+		pubKeyCMt = RSAPublicKeyReader.getPubKeyFormFile(pubkCMt);
+		target = cmTsignature;
+		ValidateSignature input = new ValidateSignature(pubKeyCMt, target);
+		
+		if(input.Validate()){
+			auth = true;
+		}else{
+			auth = false;
+		}
 		
 		
-		return procede;
+		
+		return auth;
+		
 
 	}
+
 
 }
