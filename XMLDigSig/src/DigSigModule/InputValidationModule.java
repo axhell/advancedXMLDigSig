b276e1b815a99d93e130727615b93757747c301b
axhell <edro.hi.ammen@gmail.com>
2015-01-27 10:48:23 +0100

    collection of modules that implement the requirements for a trusted certification process

---
+++ XMLDigSig/src/DigSigModule/InputValidationModule.java
@@ -0,0 +1,20 @@
+package DigSigModule;
+
+public class InputValidationModule {
+
+	/**
+	 * @param args
+	 */
+	public static boolean AuthorizationCMInstanceCreation(String cmTsignature, String cmTpubk, String fwpubk) {
+		//
+		boolean procede = false;
+		
+		
+		
+		
+		
+		return procede;
+
+	}
+
+}
