1f1daa03cb4b0f2054e7b2c4053e85f4ba3eaab7
axhell <edro.hi.ammen@gmail.com>
2015-03-12 13:55:06 +0100

    working template signature generation

--- XMLDigSig/src/xmlsigmodule/XMLSignatureGenModule.java
+++ XMLDigSig/src/xmlsigmodule/XMLSignatureGenModule.java
@@ -1,10 +1,25 @@
 package xmlsigmodule;
 
 import java.io.*;
+import java.net.URLEncoder;
 import java.security.PrivateKey;
+import java.security.cert.X509Certificate;
 
 import javax.security.cert.CertificateEncodingException;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
+
+
+
+
+//import org.apache.xml.security.stax.ext.Transformer;
+import org.w3c.dom.Document;
+
+import xades4j.XAdES4jException;
 import xades4j.algorithms.XPath2FilterTransform.XPath2Filter;
 import xades4j.production.*;
 import xades4j.properties.DataObjectDesc;
@@ -57,32 +72,26 @@ public class XMLSignatureGenModule {
 		try {
 			inStream = new FileInputStream(certUserfile);
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		certUser = new X509CertificateValidation(inStream);
 		
 		PrivateKey privKCA = null;
 		try {
-			privKCA = RSAPrivateKeyReader.getPrivKeyFromFile("cert/rooCA.der");
+			privKCA = RSAPrivateKeyReader.getPrivKeyFromFile("cert/ca_rsa_priveKey.der");
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
-		KeyingDataProvider kp = new DirectKeyingDataProvider(certCA.cert, privKCA);
-		XadesSigningProfile p = new XadesBesSigningProfile(kp);
-		try {
-			XadesSigner signer = p.newSigner();
-		} catch (XadesProfileResolutionException e) {
-			e.printStackTrace();
-		}
 		
-		DataObjectDesc cmtemp = new DataObjectReference("CMtemp.xml")
-				.withTransform(XPath2Filter.intersect("/"))
-				.withDataObjectFormat(new DataObjectFormatProperty("application/xml"))//MimeTipe qualify
-				.withDataObjectTimeStamp();//timestamp qualify
-		SignedDataObjects objs = new SignedDataObjects(cmtemp);
+		
+		XadesSigner signer = getSigner(certCA.cert, privKCA);
+		
+		String cmtpath = "file://C:/Users/axhell/Documents/Github/XMLDigitalSignature/XMLDigSig/CMtemp.xml";
+		//cmtpath = URLEncoder.encode(cmtpath, "UTF-8"); 
+		signXAdESBES(cmtpath, signer);
+		
+		
 		//CM template verifica
 		//System.out.println("Certification Model Template (URI): ");
 		//Altri input?
@@ -92,10 +101,8 @@ public class XMLSignatureGenModule {
 		
 		inStream.close();
 		
-		//output risultati
-		//certCA.show();
+		
 		certCA.ValidateRootCA(certCA.cert);
-		//certUser.show();
 		certUser.Validate(certCA.cert);
 		
 		
@@ -104,6 +111,75 @@ public class XMLSignatureGenModule {
 		//Canonicali
 		
 		//Firma
+		
+	}
+
+	private static void signXAdESBES(String string, XadesSigner signer) {
+		
+		DataObjectDesc cmtemp = new DataObjectReference(string)
+		.withTransform(XPath2Filter.intersect("/"))
+		.withDataObjectFormat(new DataObjectFormatProperty("application/xml"))//MimeTipe qualify
+		;//timestamp qualify
+		SignedDataObjects objs = new SignedDataObjects(cmtemp);
+		
+		// Create the Document that will hold the resulting XMLSignature
+        DocumentBuilderFactory sigdbf = DocumentBuilderFactory.newInstance();
+        sigdbf.setNamespaceAware(true); // must be set
+        Document sigdoc = null;
+        try {
+			sigdoc = sigdbf.newDocumentBuilder().newDocument();
+		} catch (ParserConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+        
+        try {
+			XadesSignatureResult result = signer.sign(objs, sigdoc);
+		} catch (XAdES4jException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+        
+        // output the resulting document
+        OutputStream os2 = null;
+        try {
+			os2 = new FileOutputStream("CMTSignature");
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+        //os2 = System.out;
+
+        TransformerFactory tf = TransformerFactory.newInstance();
+        Transformer trans = null;
+		try {
+			trans = tf.newTransformer();
+		} catch (TransformerConfigurationException e) {
+			e.printStackTrace();
+		}
+        trans.setOutputProperty(OutputKeys.INDENT, "yes");
+        try {
+			trans.transform(new DOMSource(sigdoc), new StreamResult(os2));
+		} catch (TransformerException e) {
+			e.printStackTrace();
+		}
+        
+        
+	}
+
+	private static XadesSigner getSigner(X509Certificate cert,
+			PrivateKey privKCA) {
+		try {
+			KeyingDataProvider kp = new DirectKeyingDataProvider(cert, privKCA);
+			XadesSigningProfile p = new XadesBesSigningProfile(kp);
+			return p.newSigner();
+			} catch (XadesProfileResolutionException e) {
+				e.printStackTrace();
+			}
+		return null;
+		
 		
 	}
 }
