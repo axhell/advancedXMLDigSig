a9d671279f177d4132a2958ba7728e049011ce3e
axhell <edro.hi.ammen@gmail.com>
2015-05-07 19:04:45 +0200

    add comments

--- XMLDigSig/src/xmlsigmodule/Test.java
+++ XMLDigSig/src/xmlsigmodule/Test.java
@@ -14,13 +14,13 @@ public class Test {
 	public static void main(String[] args) throws Exception {
 		
 		
-		String certCAfile = null; //CA's certificate file path.
-		String certUserfile = null; //User's certificate file path.
-		String cmtempfn = null; //CM template file name.
-		String cminstfn = null; //CM instance file name.
-		String cmtsignatureFN = null; //CM template signature file path.
+		String certCAfile = null; //CA's certificate file relative path.
+		String certUserfile = null; //User's certificate file relative path.
+		String cmtempfn = null; //CM template file relative path.
+		String cminstfn = null; //CM instance file relative path.
+		String cmtsignatureFN = null; //CM template signature file relative path.
 		String absolutePath = null; //URI for both CM Template and Instance.
-		String privKuser = null; //User private key file path.
+		String privKuser = null; //User private key file relative path.
 		
 		
 		CMInstanceSignatureGenModule cmiSignature = new CMInstanceSignatureGenModule(certCAfile, certUserfile, cmtempfn, cminstfn, cmtsignatureFN, absolutePath, privKuser);
